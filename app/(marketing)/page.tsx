6ddb901ae30c689af8090cdda42fa28b7ef8b4eb
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-16 17:22:29 +0900

    ??

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,10 +4,13 @@ export default function IndexPage() {
   return (
     <>
     <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
-        <div className="container text-center  flex flex-col items-center gap-4 max-w-[64rem]">
-            <Link href={"/"}>Xをフォローする</Link>
-            <h1>Post Writer</h1>
-            <p>このアプリケーションはNext.jsAppRouterで作られたものです。ユーザーは自由にポストすることができます。</p>
+        <div className="container text-center flex flex-col items-center gap-4 ">
+          <Link href={"/"}>Xをフォローする</Link>
+          <h1>Post Writer</h1>
+          <p>
+            このアプリケーションはNext.js
+            AppRouterで作成されたものです。ユーザーは自由にポストを作成することができます。
+          </p>
         </div>
     </section>
     </>
