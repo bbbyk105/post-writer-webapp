322f7ae0d3209641dd1d52c164c37ef074922b7b
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-16 18:34:13 +0900

    tsの宣言と型定義、リンクの追加

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,5 @@
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
@@ -9,7 +10,7 @@ export default function IndexPage() {
         <div className="text-center">
           <div className="container mx-auto flex flex-col items-center gap-4 max-w-[64rem]">
             <Link
-              href={"https://x.com/byakko_bbb"}
+              href={siteConfig.links.x}
               className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm"
               target="_blank"
               rel="noreferrer"
@@ -31,7 +32,7 @@ export default function IndexPage() {
                 はじめる
               </Link>
               <Link
-                href={"/"}
+                href={siteConfig.links.github}
                 className={cn(
                   buttonVariants({ size: "lg", variant: "outline" })
                 )}
---
+++ config/site.tsx
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types/index.d";
+
+export const siteConfig: SiteConfig = {
+    name: "postWriter",
+    description:"",
+    url:"http://localhost:3000",
+    onImage:"",
+    links :{
+        x:"https://x.com/byakko_bbb",
+        github:"https://github.com/bbbyk105",
+    },
+};
