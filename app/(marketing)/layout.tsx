22a1aea8d1a0d68de1a2e7cc6598f573e2c3d3e1
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-17 22:54:38 +0900

    header完成

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,6 +1,7 @@
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import MainNav from "@/components/main-nav";
 
 export default function MarketingLayout({
   children,
@@ -9,8 +10,9 @@ export default function MarketingLayout({
 }) {
   return (
     <div>
-      <header className="container z-40 bg-background">
-        <div className="h-20 py-6">
+      <header className="container mx-auto z-40 bg-background">
+        <div className="h-20 py-6 flex items-center justify-between">
+          <MainNav />
           <nav>
             <Link 
               href={"/login"}
