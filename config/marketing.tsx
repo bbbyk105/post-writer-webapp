26412e663d7d88106f2639f4fbd272df8b220580
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-18 16:46:50 +0900

    headerNavの情報を別ファイルに移動

---
+++ config/marketing.tsx
@@ -0,0 +1,18 @@
+import { MarketingConfig } from "@/types/index.d";
+
+export const marketingConfig: MarketingConfig = {
+  mainNav: [
+    {
+      title: "特徴",
+      href: "#features",
+    },
+    {
+      title: "ブログ",
+      href: "/blog",
+    },
+    {
+      title: "価格",
+      href: "/pricing",
+    },
+  ],
+};
