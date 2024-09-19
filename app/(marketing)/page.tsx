84d8c408a3102161c4a62f53edcb1904ab75c313
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-19 23:51:35 +0900

    cardの追加

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -46,10 +46,41 @@ export default function IndexPage() {
         </div>
       </section>
 
-      <section id="features" className="container mx-auto py-8 md:py-12 lg:py-24 bg-slate-50">
+      <section
+        id="features"
+        className="container mx-auto py-8 md:py-12 lg:py-24 bg-slate-50 space-y-6"
+      >
         <div className="text-center space-y-6 max-w-[58rem] mx-auto">
-          <h2 className="font-extrabold text-3xl md:text-6xl">サービスの特徴</h2>
-          <p className="text-muted-foreground sm:text-lg sm:leading-7">このプロジェクトはモダンな技術スタックを用いて作られたWebアプリケーションです。Next.jsやAppRouterを利用してマークダウン形式でブログ投稿ができます。</p>
+          <h2 className="font-extrabold text-3xl md:text-6xl">
+            サービスの特徴
+          </h2>
+          <p className="text-muted-foreground sm:text-lg sm:leading-7">
+            このプロジェクトはモダンな技術スタックを用いて作られたWebアプリケーションです。Next.jsやAppRouterを利用してマークダウン形式でブログ投稿ができます。
+          </p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
+          <div className="bg-background border p-2 rounded-lg">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="45"
+                height="45"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#212121"
+                  d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+                ></path>
+              </svg>
+              <div>
+                <h3>Next.js</h3>
+                <p>AppRouter/Layouts/APIRoutersの技術を使用</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
