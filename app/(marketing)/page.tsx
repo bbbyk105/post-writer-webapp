597e677c60869f934c0db0d177df7877a66f7e5d
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-20 21:08:31 +0900

    iconセクション

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -59,9 +59,9 @@ export default function IndexPage() {
           </p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto max-w-[64rem]">
           <div className="bg-background border p-2 rounded-lg">
-            <div>
+            <div className="flex flex-col justify-center p-6 h-[180px] ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -75,9 +75,119 @@ export default function IndexPage() {
                   d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
                 ></path>
               </svg>
-              <div>
-                <h3>Next.js</h3>
-                <p>AppRouter/Layouts/APIRoutersの技術を使用</p>
+              <div className="space-y-2">
+                <h3 className="font-bold ">Next.js</h3>
+                <p className="text-sm text-muted-foreground">AppRouter/Layouts/APIRoutersの技術を使用</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-background border p-2 rounded-lg">
+            <div className="flex flex-col justify-center p-6 h-[180px] ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="45"
+                height="45"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#212121"
+                  d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+                ></path>
+              </svg>
+              <div className="space-y-2">
+                <h3 className="font-bold ">Next.js</h3>
+                <p className="text-sm text-muted-foreground">AppRouter/Layouts/APIRoutersの技術を使用</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-background border p-2 rounded-lg">
+            <div className="flex flex-col justify-center p-6 h-[180px] ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="45"
+                height="45"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#212121"
+                  d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+                ></path>
+              </svg>
+              <div className="space-y-2">
+                <h3 className="font-bold ">Next.js</h3>
+                <p className="text-sm text-muted-foreground">AppRouter/Layouts/APIRoutersの技術を使用</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-background border p-2 rounded-lg">
+            <div className="flex flex-col justify-center p-6 h-[180px] ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="45"
+                height="45"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#212121"
+                  d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+                ></path>
+              </svg>
+              <div className="space-y-2">
+                <h3 className="font-bold ">Next.js</h3>
+                <p className="text-sm text-muted-foreground">AppRouter/Layouts/APIRoutersの技術を使用</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-background border p-2 rounded-lg">
+            <div className="flex flex-col justify-center p-6 h-[180px] ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="45"
+                height="45"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#212121"
+                  d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+                ></path>
+              </svg>
+              <div className="space-y-2">
+                <h3 className="font-bold ">Next.js</h3>
+                <p className="text-sm text-muted-foreground">AppRouter/Layouts/APIRoutersの技術を使用</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-background border p-2 rounded-lg">
+            <div className="flex flex-col justify-center p-6 h-[180px] ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="45"
+                height="45"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  fill="#212121"
+                  d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+                ></path>
+              </svg>
+              <div className="space-y-2">
+                <h3 className="font-bold ">Next.js</h3>
+                <p className="text-sm text-muted-foreground">AppRouter/Layouts/APIRoutersの技術を使用</p>
               </div>
             </div>
           </div>
