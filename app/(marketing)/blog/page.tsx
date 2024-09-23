987d25a2cd147bff072c4cd78b0167c425429a8b
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-23 14:31:23 +0900

    ブログの内容表示

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -1,12 +1,42 @@
-import {allPosts} from "@/.contentlayer/generated"
+import Image from "next/image"; // Image コンポーネントのインポート
+import { allPosts } from "@/.contentlayer/generated";
 
 export default function BlogPage() {
-const posts = allPosts;
-console.log(posts)
+  const posts = allPosts;
 
-    return (
-        <div>
-            <div>Blog page</div>
+  return (
+    <div className="container mx-auto max-w-4xl py-6 lg:py-10">
+      <div>
+        <div className="space-y-4">
+          <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">
+            Blog🚀
+          </h1>
+          <p className="text-muted-foreground text-xl">
+            ContentLayerとMDXで書いています
+          </p>
         </div>
-    )
+      </div>
+      <hr className="my-8" />
+      {posts.map((post) => (
+        <article key={post._id} className="my-4">
+          {post.image && (
+            <Image
+              src={post.image}
+              alt={post.title}
+              width={804}
+              height={452}
+              className="rounded-md border bg-muted"
+            />
+          )}
+          <h2 className="font-extrabold text-2xl">{post.title}</h2>
+          {post.description && (
+            <p className="text-muted-foreground">{post.description}</p>
+          )}
+          {post.date && (
+            <p className="text-sm text-muted-foreground">{post.date}</p>
+          )}
+        </article>
+      ))}
+    </div>
+  );
 }
