aa4bab94ac27b1b4198df41956b26ae6dec0cef5
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-01-14 13:00:29 +0900

    Mdx componentの作成

--- app/(marketing)/blog/[slug]/page.tsx
+++ app/(marketing)/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { allPosts } from "@/.contentlayer/generated";
+import Mdx from "@/components/mdx-component";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
@@ -43,7 +44,7 @@ export default async function PostPage({
           className="my-8 border rounded-md bg-muted"
         />
       )}
-      <div>{post.body.html}</div>
+      <Mdx code={post.body.code} />
       <hr className="mt-12" />
       <div className="py-6 text-center lg:py-10">
         <Link
