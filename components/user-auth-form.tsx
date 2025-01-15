9e7ec9e58411db2dc120fe6f23890ecfe2a7ab56
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-01-15 15:32:21 +0900

    ログインページの作成

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 
 export default function UserAuthForm() {
   return (
-    <div>
+    <div className="grid gap-6">
       <form>
         <div className="grid gap-2">
           <div className="grid gap-1">
@@ -17,6 +17,20 @@ export default function UserAuthForm() {
           </button>
         </div>
       </form>
+
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs">
+          <span className="text-muted-foreground px-2 bg-background">
+            または
+          </span>
+        </div>
+      </div>
+      <button className={cn(buttonVariants({ variant: "outline" }))}>
+        Github
+      </button>
     </div>
   );
 }
