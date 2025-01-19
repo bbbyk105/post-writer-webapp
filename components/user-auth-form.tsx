d6cc76c711c341fc4e4bddd7a336de72f854d6db
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-01-20 01:24:48 +0900

    google settionの作成

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 
 export default function UserAuthForm() {
   const [isGithubLoading, setIsGithubLoading] = useState<boolean>(false);
+  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false);
 
   return (
     <div className="grid gap-6">
@@ -34,20 +35,36 @@ export default function UserAuthForm() {
           </span>
         </div>
       </div>
-      <button
-        className={cn(buttonVariants({ variant: "outline" }))}
-        onClick={() => {
-          setIsGithubLoading(true);
-          signIn("github");
-        }}
-      >
-        {isGithubLoading ? (
-          <Icon.spinner className="mr-2 animate-spin" />
-        ) : (
-          <Icon.github className="mr-2" />
-        )}
-        Github
-      </button>
+      <div className="flex flex-col gap-3">
+        <button
+          className={cn(buttonVariants({ variant: "outline" }))}
+          onClick={() => {
+            setIsGithubLoading(true);
+            signIn("github");
+          }}
+        >
+          {isGithubLoading ? (
+            <Icon.spinner className="mr-2 animate-spin" />
+          ) : (
+            <Icon.github className="mr-2" />
+          )}
+          Github
+        </button>
+        <button
+          className={cn(buttonVariants({ variant: "outline" }))}
+          onClick={() => {
+            setIsGoogleLoading(true);
+            signIn("google");
+          }}
+        >
+          {isGoogleLoading ? (
+            <Icon.spinner className="mr-2 animate-spin" />
+          ) : (
+            <Icon.google className="mr-2" />
+          )}
+          Google
+        </button>
+      </div>
     </div>
   );
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -7,7 +8,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
-    // Google ({})
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
 
   pages: {
